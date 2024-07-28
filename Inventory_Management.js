b58c093e93b5c0c0cb6d3cf675e39d402043bad9
Stack Soumik <sarkarsoumik215@gmail.com>
2024-07-28 23:33:27 +0600

    add problem doc

--- Inventory_Management.js
+++ Inventory_Management.js
@@ -1,3 +1,22 @@
+//intermediate-level JavaScript problem  
+
+// Problem: Inventory Management
+// You are tasked with creating a simple inventory management system for a small store. The system should be able to add, remove, and update items in the inventory. Each item in the inventory has the following properties:
+
+// id: a unique identifier for the item
+// name: the name of the item
+// quantity: the number of items available in the inventory
+// price: the price of the item
+// Your goal is to implement the following functions:
+
+// addItem(inventory, item): Adds a new item to the inventory. If an item with the same id already exists, it should not add the item and should return false. Otherwise, it should add the item and return true.
+
+// removeItem(inventory, itemId): Removes an item from the inventory by its id. If the item does not exist, it should return false. Otherwise, it should remove the item and return true.
+
+// updateItem(inventory, itemId, updates): Updates the properties of an existing item. The updates object may contain any subset of the item's properties (name, quantity, price). If the item does not exist, it should return false. Otherwise, it should update the item and return true.
+
+// getItem(inventory, itemId): Retrieves an item from the inventory by its id. If the item does not exist, it should return null. Otherwise, it should return the item.
+
 function addItem(inventory, item) {
     if (inventory.some(i => i.id === item.id)) {
         return false;
